6136e2b21e9d365bc3b2ac89859436eef71ac007
wubaddicted <igivuspam@gmail.com>
2017-04-11 20:35:13 -0400

    Update

--- entry.java
+++ entry.java
@@ -11,13 +11,13 @@ public class entry{
 	public static item[] entryList=new item[200];
 	public static int n=0;
  
-	public static void main(String[] args) {
+	public static void main(String[] args) /*It's main. what more can I say*/{
 		String selector, name;
 		instantiate();
 		Scanner stdin=new Scanner(System.in);
 		String file="/home/wmc/Documents/code/java/project6/database.txt";
 		readIn(file);
-		System.out.print("Command: ");
+		System.out.print("\033[1mCommand: ");
 		selector=stdin.next();
 		selector=selector.toLowerCase();
 		dance: for (;;) {
@@ -36,6 +36,13 @@ public class entry{
 					break;
 				case "l": list();
 					break;
+				case "r":
+				{
+					name=stdin.next();
+					remove(name);
+
+				}
+					break;
 				case "q": break dance; //YEAH!
 			}
 		System.out.print("Command: ");
@@ -50,7 +57,7 @@ public class entry{
 		System.out.println("Thanks for using this system!");
 	}
 
-	public static void readIn(String filename){
+	public static void readIn(String filename)/*Reads in the file*/{
 		try 
 		{
 			int i=0;
@@ -60,12 +67,8 @@ public class entry{
 			line=br.readLine();
 			do
 			{
-				line+=" "+br.readLine()+ " ";
-				line+=br.readLine();
-				//System.out.printf("%s: %d%n\t%s%n",entryList[i].name, entryList[i].qnty, entryList[i].notes);
 				process(line, i);
 				++n;
-				line=br.readLine()+ " ";
 				line=br.readLine();
 			}while ( ++i< 200 && line !=null);
 			br.close();
@@ -77,7 +80,7 @@ public class entry{
 			
 	}
 
-	public static void enter(String name)
+	public static void enter(String name) /* enter a new item, or update the old one*/
 	{
 		Scanner stdin=new Scanner(System.in);
 		int qty, index;
@@ -85,7 +88,8 @@ public class entry{
 		System.out.printf("Enter quantity: ");
 		qty=stdin.nextInt();
 		System.out.printf("Enter notes: ");
-		notes=stdin.next();
+		stdin.nextLine();
+		notes=stdin.nextLine();
 		index=find(name);
 		if (index == -9) //didn't find entry
 		{
@@ -101,19 +105,19 @@ public class entry{
 		return;
 	}
 
-	public static void WriteInventory(String FileName) throws Exception{
+	public static void WriteInventory(String FileName) /*Writes to the file*/throws Exception {
    PrintStream P  = new PrintStream(FileName);
 		 			
    for (int i=0; i < n; i++) {
-	P.println(entryList[i].name      + "\t" +
-	          entryList[i].qnty  + "\t" +
+	P.println(entryList[i].name      + " " +
+	          entryList[i].qnty  + " " +
 		    entryList[i].notes);
    }
    P.close();
    System.out.println("Inventory stored.");	
 }
 
-	public static int find(String to_Search)
+	public static int find(String to_Search)/* finds entry position*/
 	{
 		for (int i = 0; i < n;++i){
 		if (to_Search.equals(entryList[i].name)){
@@ -128,12 +132,12 @@ public class entry{
 			print(i);
 	}
 
-public static void print(int flamingo)
+public static void print(int flamingo) /*prints single entries to stdout*/
 {
 	System.out.printf("%s: %d%n\t->%s%n",entryList[flamingo].name, entryList[flamingo].qnty,entryList[flamingo].notes);
 }
 
-public static void process (String line, int place)
+public static void process (String line, int place) /*takes lines, pastes into class entry*/
 {
 	System.out.println(line);
 	try {
@@ -150,11 +154,23 @@ public static void process (String line, int place)
 	}
 }
 
-public static void instantiate()
+public static void instantiate() /*Just instantiates all the objects*/
 {
   for (int i=0;i <200; ++i)
   {
     entryList[i]=new item();
   }
+}
+
+public static void remove(String name) /*removes an object if it exists*/
+{
+	int pos=find(name);
+	if (pos !=-9) {
+		item temp=entryList[pos];
+		entryList[pos]=entryList[n-1];
+		entryList[n]=temp;
+		--n;
+	}
+	else System.out.println("Couldn't find item");
 }
 }
